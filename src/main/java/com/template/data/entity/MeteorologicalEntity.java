fe235f753f0498d3846375e12b338058a391f933
mateus.santana <mateus.santana@dbserver.com.br>
2023-04-25 11:06:29 -0300

    Add climate on post

--- src/main/java/com/template/data/entity/MeteorologicalEntity.java
+++ src/main/java/com/template/data/entity/MeteorologicalEntity.java
@@ -1,5 +1,6 @@
 package com.template.data.entity;
 
+import com.template.data.entity.ennum.Climate;
 import com.template.data.entity.ennum.Shift;
 import jakarta.persistence.*;
 import lombok.Data;
@@ -35,88 +36,8 @@ public class MeteorologicalEntity {
     @Column(name = "shift")
     private Shift shift;
 
-    public MeteorologicalEntity(long id, String city, Float windSpeed, Float maxTemp, Float minTemp, Float humidity, Float precipitation, Date date, Shift shift) {
-        this.id = id;
-        this.city = city;
-        this.windSpeed = windSpeed;
-        this.maxTemp = maxTemp;
-        this.minTemp = minTemp;
-        this.humidity = humidity;
-        this.precipitation = precipitation;
-        this.date = date;
-        this.shift = shift;
-    }
-    public MeteorologicalEntity(){}
+    @Column(name = "climate")
+    private Climate climate;
 
-    public long getId() {
-        return id;
-    }
 
-    public void setId(long id) {
-        this.id = id;
-    }
-
-    public String getCity() {
-        return city;
-    }
-
-    public void setCity(String city) {
-        this.city = city;
-    }
-
-    public Float getWindSpeed() {
-        return windSpeed;
-    }
-
-    public void setWindSpeed(Float windSpeed) {
-        this.windSpeed = windSpeed;
-    }
-
-    public Float getMaxTemp() {
-        return maxTemp;
-    }
-
-    public void setMaxTemp(Float maxTemp) {
-        this.maxTemp = maxTemp;
-    }
-
-    public Float getMinTemp() {
-        return minTemp;
-    }
-
-    public void setMinTemp(Float minTemp) {
-        this.minTemp = minTemp;
-    }
-
-    public Float getHumidity() {
-        return humidity;
-    }
-
-    public void setHumidity(Float humidity) {
-        this.humidity = humidity;
-    }
-
-    public Float getPrecipitation() {
-        return precipitation;
-    }
-
-    public void setPrecipitation(Float precipitation) {
-        this.precipitation = precipitation;
-    }
-
-    public Date getDate() {
-        return date;
-    }
-
-    public void setDate(Date date) {
-        this.date = date;
-    }
-
-    public Shift getShift() {
-        return shift;
-    }
-
-    public void setShift(Shift shift) {
-        this.shift = shift;
-    }
 }
